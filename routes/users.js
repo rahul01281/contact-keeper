d1e07d2e40c480839cad2779c319cac8549146f4
Rahul Rinwa <rahul.rinwah@gmail.com>
2020-12-23 18:23:48 +0530

    user model and validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // @route      POST api/users
 // @desc       register a user
 // @access     public
-router.post('/', (req, res) => {
-    res.send('register a user');
+router.post('/', [
+    //sets the checks
+    check('name', 'Enter Name').not().isEmpty(),
+    check('email', 'Enter valid Email').isEmail(),
+    check('password', 'Enter a password with 6 or more characters').isLength({min: 6})
+], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('passed');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //connect databse
 connectDB();
 
+//init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.json({msg: "Contact Keeper API"}));
 
 //define routes
