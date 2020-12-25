9c9d6ef95623348f4f87c9baa26164e3cc29f611
Rahul Rinwa <rahul.rinwah@gmail.com>
2020-12-26 00:59:12 +0530

    navbar and router setup

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h1>About this App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, nam sed voluptas corrupti repellat ipsa facere enim, necessitatibus, fuga accusantium recusandae placeat aliquid veniam sequi amet illo magnam velit aperiam iusto eum. Modi, ducimus. Pariatur sapiente recusandae ex unde culpa laboriosam velit, sit necessitatibus deleniti voluptates. Libero magni repudiandae debitis.</p>
+            <p className="bg-dark p"><strong>version: 1.0.0</strong></p>
+        </div>
+    )
+}
+
+export default About
